e16206d9357fc3c022fd2c8bd5e75920b76e6093
perplexhub <18417689+perplexhub@users.noreply.github.com>
2019-01-28 15:21:30 +0800

    add @Bean annotation

--- src/main/java/io/github/perplexhub/rsql/jpa/RsqlJpaSpecificationConfig.java
+++ src/main/java/io/github/perplexhub/rsql/jpa/RsqlJpaSpecificationConfig.java
@@ -2,11 +2,13 @@ package io.github.perplexhub.rsql.jpa;
 
 import javax.persistence.EntityManager;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RsqlJpaSpecificationConfig {
 
+	@Bean
 	public RsqlJpaSpecification rsqlJpaSpecification(EntityManager entityManager) {
 		return new RsqlJpaSpecification(entityManager);
 	}
