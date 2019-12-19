fb32da2184e996eb3e67174680b8b79d65484e3f
Marten <marten@rocketbase.io>
2019-12-19 11:42:38 +0100

    added element collection sample

--- src/test/java/io/github/perplexhub/rsql/model/Company.java
+++ src/test/java/io/github/perplexhub/rsql/model/Company.java
@@ -1,11 +1,11 @@
 package io.github.perplexhub.rsql.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -13,11 +13,20 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Company {
 
-	@Id
-	private Integer id;
+    @Id
+    private Integer id;
 
-	private String code;
+    private String code;
 
-	private String name;
+    private String name;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "company_tags",
+            joinColumns = @JoinColumn(name = "id", referencedColumnName = "id"),
+            uniqueConstraints = @UniqueConstraint(name = "uk_company_tag", columnNames = {"id", "tag"})
+    )
+    @Column(name = "tag", length = 20, nullable = false)
+    private List<String> tags;
 
 }
