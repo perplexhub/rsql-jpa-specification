61d6cf4c7fb527299efa57da23f566a9b3d45492
Rakesh Roshan <rakesh.roshan007@gmail.com>
2021-09-16 06:32:28 -0400

    Add support for ignore case

--- rsql-common/src/test/resources/import_company.sql
+++ rsql-common/src/test/resources/import_company.sql
@@ -1,7 +1,7 @@
 insert into company(id, code, name) values(1, 'hello', 'Hello Inc');
-insert into company(id, code, name) values(2, 'world', 'World Inc');
+insert into company(id, code, name) values(2, 'World', 'World Inc');
 insert into company(id, code, name) values(3, 'demo', 'Demo Central');
-insert into company(id, code, name) values(4, 'test', 'Test Lab');
+insert into company(id, code, name) values(4, 'Test', 'Test Lab');
 insert into company(id, code, name) values(5, 'fake', 'Fake Company');
 insert into company(id, code, name) values(6, 'empty', '');
 insert into company(id, code, name) values(7, 'null', NULL);
