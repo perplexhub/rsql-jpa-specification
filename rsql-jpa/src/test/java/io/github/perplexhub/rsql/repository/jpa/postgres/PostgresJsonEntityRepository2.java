d3843393870623bbf9f18b418f97653fa6231b95
Alexandre Boyer <33391039+ng-galien@users.noreply.github.com>
2023-11-25 19:11:22 +0100

    Cast jsonb to test when searching on root

--- rsql-jpa/src/test/java/io/github/perplexhub/rsql/repository/jpa/postgres/PostgresJsonEntityRepository2.java
+++ rsql-jpa/src/test/java/io/github/perplexhub/rsql/repository/jpa/postgres/PostgresJsonEntityRepository2.java
@@ -1,10 +1,12 @@
 package io.github.perplexhub.rsql.repository.jpa.postgres;
 
 import io.github.perplexhub.rsql.model.PostgresJsonEntity;
-import java.util.UUID;
+import io.github.perplexhub.rsql.model.PostgresJsonEntity2;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PostgresJsonEntityRepository extends JpaRepository<PostgresJsonEntity, UUID>,
-    JpaSpecificationExecutor<PostgresJsonEntity> {
+import java.util.UUID;
+
+public interface PostgresJsonEntityRepository2 extends JpaRepository<PostgresJsonEntity2, UUID>,
+    JpaSpecificationExecutor<PostgresJsonEntity2> {
 }
